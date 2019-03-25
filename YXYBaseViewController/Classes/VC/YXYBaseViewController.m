9fee476630dbb17c946a678d0facfe9bd60a5e6a
yangxiaoyu <576842121@qq.com>
2019-03-25 17:42:48 +0800

    fix 检测VC内存泄漏

--- YXYBaseViewController/Classes/VC/YXYBaseViewController.m
+++ YXYBaseViewController/Classes/VC/YXYBaseViewController.m
@@ -73,7 +73,7 @@
     __weak YXYBaseViewController *weakSelf = self;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         if (weakSelf) {
-            NSString *title = [NSString stringWithFormat:@"%@没有被释放", NSStringFromClass(self.class)];
+            NSString *title = [NSString stringWithFormat:@"%@没有被释放", NSStringFromClass(weakSelf.class)];
             [YXYAlertView alertWithTitle:title completion:nil];
         }
     });
